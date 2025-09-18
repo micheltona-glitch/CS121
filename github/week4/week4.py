f5555eb725c6d63de75330922d960c6c4b360867
micheltona-glitch <sebakaratona@gmail.com>
2025-09-18 12:21:44 -0500

    Create week4.py
    1 to 4

---
+++ github/week4/week4.py
@@ -0,0 +1,34 @@
+#Q2 
+'''word = input("enter a word ")
+result = " "
+for i in range(1 , len(word) - 1,2):
+    result += word[i]
+print(f"output = {result}") 
+'''   
+#Q1
+'''
+larger_num = int(input("enter the larger number"))
+smaller_num = int(input("entere the smaller number"))
+num = 0
+while larger_num > smaller_num :
+    larger_num /= 2
+    num += 1
+print(f"Number of times halved:{num} ")    '''
+
+#Q3
+'''
+for i in range(37, 1050, 1):
+    if i % 2 == 0 :
+        print(i)
+    else:
+        " "    
+'''
+#Q4
+
+'''word= " "
+while True:
+    letter = input("enter a letter ")
+    word += letter
+    if letter == "done":
+        break
+print(word)    '''
